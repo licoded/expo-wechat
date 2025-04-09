b53fc3c9c01789b8bdf3de576e668c64c990eaf1
weiyang333 <3425495909@qq.com>
2025-04-09 20:00:09 +0800

    feat: finish person-info page

---
+++ app/screens/PersonInfoScreen.js
@@ -0,0 +1,150 @@
+import React, {Component} from 'react';
+import {
+  Dimensions, 
+  Image, 
+  StyleSheet, 
+  Text, 
+  View
+} from 'react-native';
+
+const {width} = Dimensions.get('window');
+
+export default class PersonInfoScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* 静态标题栏 */}
+        <View style={styles.titleBar}>
+          <Text style={styles.titleText}>个人信息</Text>
+        </View>
+
+        {/* 静态内容列表 */}
+        <View style={styles.list}>
+          {/* 头像 */}
+          <View style={styles.listItem}>
+            <Text style={styles.listItemLeftText}>头像</Text>
+            <View style={styles.rightContainer}>
+              <Image 
+                style={[styles.listItemRight, styles.avatarImg]}
+                source={require('../../assets/images/avatar.png')}
+              />
+            </View>
+          </View>
+          
+          <View style={styles.divider}/>
+          
+          {/* 昵称 */}
+          <View style={styles.listItem}>
+            <Text style={styles.listItemLeftText}>昵称</Text>
+            <View style={styles.rightContainer}>
+              <Text>用户昵称</Text>
+            </View>
+            <Image 
+              source={require('../../assets/images/ic_right_arrow.png')} 
+              style={styles.rightArrow}
+            />
+          </View>
+          
+          <View style={styles.divider}/>
+          
+          {/* 微信号 */}
+          <View style={styles.listItem}>
+            <Text style={styles.listItemLeftText}>微信号</Text>
+            <View style={styles.rightContainer}>
+              <Text>user123</Text>
+            </View>
+          </View>
+          
+          <View style={styles.divider}/>
+          
+          {/* 二维码名片 */}
+          <View style={styles.listItem}>
+            <Text style={styles.listItemLeftText}>二维码名片</Text>
+            <View style={styles.rightContainer}>
+              <Image 
+                style={[styles.listItemRight, styles.qrcodeImg]} 
+                source={require('../../assets/images/ic_qr_code.png')}
+              />
+            </View>
+          </View>
+          
+          <View style={styles.divider}/>
+          
+          {/* 更多 */}
+          <View style={styles.listItem}>
+            <Text style={styles.listItemLeftText}>更多</Text>
+          </View>
+          
+          {/* 间距 */}
+          <View style={{height: 20, width: width}}/>
+          
+          {/* 我的地址 */}
+          <View style={styles.listItem}>
+            <Text style={styles.listItemLeftText}>我的地址</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#f5f5f5'
+  },
+  titleBar: {
+    height: 50,
+    justifyContent: 'center',
+    paddingLeft: 15,
+    backgroundColor: '#fff'
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#e0e0e0',
+    marginLeft: 15
+  },
+  list: {
+    flex: 1,
+    flexDirection: 'column',
+    marginTop: 20,
+    backgroundColor: '#fff'
+  },
+  listItem: {
+    flexDirection: 'row',
+    padding: 15,
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF'
+  },
+  listItemLeftText: {
+    color: '#000000',
+    fontSize: 16,
+  },
+  rightContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  listItemRight: {
+    alignItems: 'flex-end'
+  },
+  avatarImg: {
+    width: 60,
+    height: 60,
+    borderRadius: 30
+  },
+  qrcodeImg: {
+    width: 25,
+    height: 25,
+  },
+  rightArrow: {
+    width: 8,
+    height: 14,
+    marginLeft: 10
+  }
+});
