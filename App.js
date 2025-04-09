628fa5f89a620bd039f3d3a076c387aed9fb54a7
weiyang333 <3425495909@qq.com>
2025-04-09 20:00:09 +0800

    feat: finish person-info page

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import LoginScreen from './app/screens/LoginScreen';
 import ContactsScreen from './app/screens/ContactsScreen';
 import ChattingScreen from './app/screens/ChattingScreen';
+import PersonInfoScreen from './app/screens/PersonInfoScreen';
 
 import { InitIM } from './app/utils/IMUtils';
 
 InitIM();
 
-export default ContactsScreen;
+export default PersonInfoScreen;
