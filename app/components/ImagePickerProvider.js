f356cc8bb3733a00db1795714e23601c5ec66b08
ys <yayea@foxmail.com>
2025-04-11 14:49:25 +0800

    app: add ImagePickerProvider component for image selection and upload

---
+++ app/components/ImagePickerProvider.js
@@ -0,0 +1,115 @@
+// ImagePickerProvider.js
+import React from 'react'
+import * as ImagePicker from 'expo-image-picker'
+import { post } from '../utils/AxiosUtil'
+
+/**
+ * @component ImagePickerProvider
+ * @description 图片选择器组件，支持选择及上传图片，并通过 render props 提供相关方法
+ *
+ * @param {Object} props - 组件属性
+ * @param {boolean} props.allowsEditing - 是否允许编辑图片，默认为true
+ * @param {boolean} props.allowsMultipleSelection - 是否允许多选图片，默认为false
+ * @param {number} props.selectionLimit - 多选时的图片数量限制，设置为0表示系统支持的最大选择限制，默认为1
+ * @param {Function} props.beforeUpload - 上传前的处理函数，返回处理后的图片数组或者假值(取消上传)
+ * @param {boolean} props.autoUpload - 是否自动上传图片，默认为true
+ * @param {string} props.uploadUrl - 图片上传的接口地址
+ * @param {Function} props.onSuccess - 上传成功后的回调函数，参数为上传结果数组
+ *
+ * @returns {React.ReactNode} - 返回一个包含相关方法的组件
+ * @method selectImages - 选择图片
+ * @method uploadImages - 上传图片
+ * @method clearImages - 清空图片
+ * @property selectedImages - 已选择的图片
+ */
+export class ImagePickerProvider extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedImages: []
+    }
+  }
+
+  selectImages = async () => {
+    const {
+      allowsEditing = true,
+      allowsMultipleSelection = false,
+      selectionLimit = 1,
+      beforeUpload,
+      autoUpload = true,
+      uploadUrl,
+      onSuccess
+    } = this.props
+
+    // 选择图片
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: 'images',
+      allowsEditing,
+      allowsMultipleSelection,
+      selectionLimit
+    })
+
+    if (result.canceled) return
+
+    let selectedAssets = result.assets || []
+
+    // 上传前处理：如果beforeUpload返回假值，则不继续上传
+    if (beforeUpload && typeof beforeUpload === 'function') {
+      const processedAssets = await beforeUpload(selectedAssets)
+      if (!processedAssets) return
+      selectedAssets = processedAssets
+    }
+
+    // 更新值
+    this.setState({ selectedImages: selectedAssets })
+
+    // 自动上传
+    if (autoUpload && uploadUrl) {
+      this.uploadImages(selectedAssets, uploadUrl, onSuccess)
+    }
+  }
+
+  // 上传方法
+  uploadImages = async (
+    images = this.state.selectedImages,
+    url = this.props.uploadUrl,
+    callback = this.props.onSuccess
+  ) => {
+    if (!url || images.length === 0) return
+
+    const uploadPromises = images.map(async image => {
+      const formData = new FormData()
+      formData.append('file', image.file)
+
+      try {
+        const data = await post(url, formData)
+        // TODO: 上传头像接口并未返回上传后的路径或文件名，沿用kamaChat中的拼接方式
+        return data || '/static/avatars/' + image.fileName
+      } catch (error) {
+        console.error('上传图片失败:', error)
+      }
+    })
+
+    const results = await Promise.all(uploadPromises)
+    console.log('uploadImages Promise.all results', results)
+
+    if (callback && typeof callback === 'function') {
+      callback(results)
+    }
+
+    return results
+  }
+
+  clearImages = () => {
+    this.setState({ selectedImages: [] })
+  }
+
+  render() {
+    return this.props.children({
+      selectImages: this.selectImages,
+      uploadImages: this.uploadImages,
+      clearImages: this.clearImages,
+      selectedImages: this.state.selectedImages
+    })
+  }
+}
--- app/screens/ContactsScreen.js
+++ app/screens/ContactsScreen.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import Global from "../utils/Global";
+import React, { Component } from 'react'
+import Global from '../utils/Global'
 import {
   Dimensions,
   FlatList,
@@ -9,53 +9,55 @@ import {
   Text,
   TouchableHighlight,
   View,
-} from "react-native";
-import { post } from "../utils/AxiosUtil";
-import { getAvatorUrl } from "../utils/StaticUtil";
+  TouchableOpacity
+} from 'react-native'
+import { post } from '../utils/AxiosUtil'
+import { getAvatorUrl } from '../utils/StaticUtil'
+import { ImagePickerProvider } from '../components/ImagePickerProvider'
 
-const { width } = Dimensions.get("window");
+const { width } = Dimensions.get('window')
 
 const mockContactList = [
   {
-    pinyin: "LiSi",
-    name: "李四",
-    nick: "李四",
+    pinyin: 'LiSi',
+    name: '李四',
+    nick: '李四'
   },
   {
-    pinyin: "LiSi",
-    name: "张三",
-    nick: "张三",
-  },
-];
+    pinyin: 'LiSi',
+    name: '张三',
+    nick: '张三'
+  }
+]
 
 export default class ContactsScreen extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      contactData: [],
-    };
+      contactData: []
+    }
   }
 
   UNSAFE_componentWillMount() {
-    const params = { owner_id: Global.userId };
-    post("/contact/getUserList", params).then((contactData) => {
-      this.setState({ contactData });
-    });
+    const params = { owner_id: Global.userId }
+    post('/contact/getUserList', params).then(contactData => {
+      this.setState({ contactData })
+    })
   }
 
   render() {
-    var listData = [];
-    var contacts = this.state.contactData;
-    var index = 0;
+    var listData = []
+    var contacts = this.state.contactData
+    var index = 0
     for (var i = 0; i < contacts.length; i++) {
-      var pinyin = contacts[i].pinyin || contacts[i].user_name;
-      var firstLetter = pinyin.substring(0, 1);
-      if (firstLetter < "A" || firstLetter > "Z") {
-        firstLetter = "#";
+      var pinyin = contacts[i].pinyin || contacts[i].user_name
+      var firstLetter = pinyin.substring(0, 1)
+      if (firstLetter < 'A' || firstLetter > 'Z') {
+        firstLetter = '#'
       }
-      let icon = require("../../assets/images/avatar.png");
+      let icon = require('../../assets/images/avatar.png')
       if (contacts[i].avatar) {
-        icon = getAvatorUrl(contacts[i].avatar);
+        icon = getAvatorUrl(contacts[i].avatar)
       }
       listData.push({
         key: index++,
@@ -64,42 +66,42 @@ export default class ContactsScreen extends Component {
         nick: contacts[i].user_name,
         pinyin: pinyin.toUpperCase(),
         firstLetter: firstLetter,
-        sectionStart: false,
-      });
+        sectionStart: false
+      })
     }
     // 按拼音排序
     listData.sort(function (a, b) {
       if (a.pinyin === undefined || b.pinyin === undefined) {
-        return 1;
+        return 1
       }
       if (a.pinyin > b.pinyin) {
-        return 1;
+        return 1
       }
       if (a.pinyin < b.pinyin) {
-        return -1;
+        return -1
       }
-      return 0;
-    });
+      return 0
+    })
     // 根据首字母分区
     for (var i = 0; i < listData.length; i++) {
-      var obj = listData[i];
+      var obj = listData[i]
       if (obj.pinyin === undefined) {
-        continue;
+        continue
       }
       if (i > 0 && i < listData.length) {
-        var preObj = listData[i - 1];
+        var preObj = listData[i - 1]
         if (preObj.pinyin === undefined && obj.pinyin !== undefined) {
-          obj.sectionStart = true;
+          obj.sectionStart = true
         } else if (
           preObj.pinyin !== undefined &&
           obj.pinyin !== undefined &&
           preObj.firstLetter !== obj.firstLetter
         ) {
-          obj.sectionStart = true;
+          obj.sectionStart = true
         }
       }
     }
-    this.listData = listData;
+    this.listData = listData
     return (
       <View style={styles.container}>
         {/* <TitleBar nav={this.props.navigation} /> */}
@@ -113,31 +115,40 @@ export default class ContactsScreen extends Component {
         </View>
         <View style={styles.divider}></View>
       </View>
-    );
+    )
   }
 
   _getItemLayout = (data, index) => {
-    let len = data.sectionStart ? 58 : 51;
-    let dividerHeight = 1 / PixelRatio.get();
+    let len = data.sectionStart ? 58 : 51
+    let dividerHeight = 1 / PixelRatio.get()
     return {
       length: len,
       offset: (len + dividerHeight) * index,
-      index,
-    };
-  };
-
-  onListItemClick(item) {
-    console.log("TODO: 跳转到聊天页");
+      index
+    }
   }
 
-  _renderItem = (item) => {
-    var section = [];
+  onListItemClick(item) {
+    console.log('TODO: 跳转到聊天页')
+  }
+
+  updateUserInfo = async data => {
+    const [avatar] = data
+    // TODO: uuid获取方式有待修改
+    const userInfo = { uuid: 'U2025040881646893574', avatar }
+    await post('/user/updateUserInfo', userInfo)
+  }
+
+  _renderItem = item => {
+    var section = []
     if (item.item.sectionStart) {
       section.push(
-        <Text key={"section" + item.item.key} style={listItemStyle.sectionView}>
+        <Text
+          key={'section' + item.item.key}
+          style={listItemStyle.sectionView}>
           {item.item.firstLetter}
         </Text>
-      );
+      )
     }
     return (
       <View>
@@ -145,14 +156,26 @@ export default class ContactsScreen extends Component {
         <TouchableHighlight
           underlayColor={Global.touchableHighlightColor}
           onPress={() => {
-            this.onListItemClick(item);
-          }}
-        >
-          <View style={listItemStyle.container} key={"item" + item.item.key}>
-            <Image style={listItemStyle.image} source={item.item.icon} />
+            this.onListItemClick(item)
+          }}>
+          <View
+            style={listItemStyle.container}
+            key={'item' + item.item.key}>
+            <ImagePickerProvider
+              uploadUrl="/message/uploadAvatar"
+              onSuccess={this.updateUserInfo}>
+              {({ selectImages }) => (
+                <TouchableOpacity onPress={selectImages}>
+                  <Image
+                    style={listItemStyle.image}
+                    source={item.item.icon}
+                  />
+                </TouchableOpacity>
+              )}
+            </ImagePickerProvider>
             <Text style={listItemStyle.itemText}>{item.item.title}</Text>
             <Text style={listItemStyle.subText}>
-              {item.item.nick ? "" : "(" + item.item.nick + ")"}
+              {item.item.nick ? '' : '(' + item.item.nick + ')'}
             </Text>
           </View>
         </TouchableHighlight>
@@ -160,20 +183,20 @@ export default class ContactsScreen extends Component {
           style={{
             width: width,
             height: 1 / PixelRatio.get(),
-            backgroundColor: Global.dividerColor,
+            backgroundColor: Global.dividerColor
           }}
         />
       </View>
-    );
-  };
+    )
+  }
 }
 
 const listItemStyle = StyleSheet.create({
   container: {
     width: width,
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#FFFFFF",
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF'
   },
   image: {
     marginLeft: 15,
@@ -181,47 +204,47 @@ const listItemStyle = StyleSheet.create({
     marginTop: 8,
     marginBottom: 8,
     width: 35,
-    height: 35,
+    height: 35
   },
   itemText: {
     fontSize: 15,
-    color: "#000000",
+    color: '#000000'
   },
   subText: {
     fontSize: 15,
-    color: "#999999",
+    color: '#999999'
   },
   sectionView: {
     width: width,
-    backgroundColor: "rgba(0, 0, 0, 0)",
+    backgroundColor: 'rgba(0, 0, 0, 0)',
     paddingLeft: 10,
     paddingRight: 10,
     paddingTop: 2,
     paddingBottom: 2,
-    color: "#999999",
-  },
-});
+    color: '#999999'
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   divider: {
     width: width,
     height: 1 / PixelRatio.get(),
-    backgroundColor: "#D3D3D3",
+    backgroundColor: '#D3D3D3'
   },
   content: {
     flex: 1,
     width: width,
-    flexDirection: "row",
-    backgroundColor: Global.pageBackgroundColor,
+    flexDirection: 'row',
+    backgroundColor: Global.pageBackgroundColor
   },
   tabBarIcon: {
     width: 24,
-    height: 24,
-  },
-});
+    height: 24
+  }
+})
