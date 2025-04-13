4c966a0953147a8be6a0a8bea27acbb4dfd0c6a6
liyongkang5 <liyongkang5@xiaomi.com>
2025-04-13 08:54:19 +0800

    expo: config prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: ['expo', 'prettier'],
   ignorePatterns: ['/dist/*'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
